cfaba2dd04ce895c5b2a05426bf9fdd1415a6a3e
Aryel Tupinambá <aryel.tupinamba@lqdi.net>
2016-09-12 17:45:40 -0300

    Updated "gestor politico" wireframes; added new "UNICEF" access profile

--- public/js/app.js
+++ public/js/app.js
@@ -428,9 +428,10 @@
 			'agente_comunitario': {name: 'Mary Smith', type: 'Agente Comunitário', can: ['dashboard']},
 			'tecnico_verificador': {name: 'Paul Atree', type: 'Técnico Verificador', can: ['dashboard','cases']},
 			'supervisor_institucional': {name: 'John Doe', type: 'Supervisor Institucional', can: ['dashboard','cases','reports']},
-			'coordenador_operacional': {name: 'Aryel Tupinambá', type: 'Coordenador Operacional', can: ['dashboard','cases','reports','users','settings']},
-			'gestor_politico': {name: 'João das Neves', type: 'Gestor Político', can: ['dashboard','reports','users','settings']},
-			'super_administrador': {name: 'Morgan Freeman', type: 'Super Administrador', can: ['dashboard','reports','cities','users','settings']}
+			'coordenador_operacional': {name: 'Aryel Tupinambá', type: 'Coordenador Operacional', can: ['dashboard','cases','reports','users', 'users.edit','settings']},
+			'gestor_politico': {name: 'João das Neves', type: 'Gestor Político', can: ['dashboard','reports','users']},
+			'unicef': {name: 'Jane Doe', type: 'Gestor UNICEF', can: ['dashboard','reports','cities']},
+			'super_administrador': {name: 'Morgan Freeman', type: 'Super Administrador', can: ['dashboard','reports','cities','users','users.edit','settings']}
 		};
 
 		var currentType = 'coordenador_operacional';
