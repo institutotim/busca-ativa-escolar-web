6ff961fcf626df2d7ffd9274a265c94fce87d771
Aryel Tupinambá <aryel.tupinamba@lqdi.net>
2016-09-20 15:32:37 -0300

    Fixed some typos

--- resources/assets/js/controllers/credits.js
+++ resources/assets/js/controllers/credits.js
@@ -2,6 +2,8 @@
 
 	angular.module('BuscaAtivaEscolar').controller('CreditsCtrl', function ($scope, $rootScope, AppDependencies) {
 
+		console.log("Displaying app dependencies: ", AppDependencies);
+
 		$rootScope.section = 'credits';
 		$scope.appDependencies = AppDependencies;
 
