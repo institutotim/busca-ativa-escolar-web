ae96a3003d2f7fcf575e6d3d395bf6a9dc76afba
Aryel Tupinambá <aryel.tupinamba@lqdi.net>
2016-09-20 15:33:54 -0300

    Pushed missing compiled assets

--- public/js/app.js
+++ public/js/app.js
@@ -331,6 +331,8 @@
 
 	angular.module('BuscaAtivaEscolar').controller('CreditsCtrl', function ($scope, $rootScope, AppDependencies) {
 
+		console.log("Displaying app dependencies: ", AppDependencies);
+
 		$rootScope.section = 'credits';
 		$scope.appDependencies = AppDependencies;
 
@@ -432,7 +434,7 @@
 			];
 		});
 
-});
+})();
 (function() {
 
 	angular
