fd5ede8439c3d415345d9cf35f542b9c7a3107c9
Aryel Tupinambá <aryel.tupinamba@lqdi.net>
2017-03-27 19:09:55 -0300

    Preferences: added password change/reset support in user preferences (#206).

--- resources/assets/js/preferences/manage_user_preferences.js
+++ resources/assets/js/preferences/manage_user_preferences.js
@@ -8,7 +8,7 @@
 				controller: 'ManageUserPreferencesCtrl'
 			})
 		})
-		.controller('ManageUserPreferencesCtrl', function ($scope, $rootScope, ngToast, UserPreferences, StaticData) {
+		.controller('ManageUserPreferencesCtrl', function ($scope, $rootScope, ngToast, Identity, UserPreferences, PasswordReset, StaticData) {
 
 			$scope.static = StaticData;
 			$scope.settings = {};
@@ -21,6 +21,15 @@
 
 			$scope.save = function() {
 				UserPreferences.update({settings: $scope.settings}, $scope.refresh);
+			};
+
+			$scope.resetPassword = function() {
+				$scope.true = false;
+
+				PasswordReset.begin({email: Identity.getCurrentUser().email}, function (res) {
+					$scope.isLoading = false;
+					ngToast.success("Solicitação de troca realizada com sucesso! Verifique em seu e-mail o link para troca de senha.");
+				})
 			};
 
 			$scope.refresh();
